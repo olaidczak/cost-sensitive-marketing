8a84cd1575f76efd3417630693f90a3862c08fb3
Ola Idczak <olaa.idczak@gmail.com>
2026-05-22 19:14:18 +0200

    add first step of feature selection

---
+++ src/feature_selection/feature_selection.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_selection import mutual_info_classif, f_classif
+from sklearn.metrics import roc_auc_score
+from sklearn.inspection import permutation_importance
+from sklearn.ensemble import RandomForestClassifier
+from boruta import BorutaPy
+
+
+def info_features(X, y, top=50, return_raw=False):
+    n, p = X.shape
+
+    # Mutual Information
+    mi_scores = mutual_info_classif(X, y, random_state=1605)
+
+    # anova f statistic
+    f_scores, f_pvalues = f_classif(X, y)
+
+    # auc per variable
+    auc_scores = np.zeros(p)
+    for i in range(p):
+        col = X.iloc[:, i]
+        auc = roc_auc_score(y, col)
+        auc_scores[i] = max(auc, 1 - auc)
+
+    # boruta
+    X_tmp = X.values
+    y_tmp = y
+    rf = RandomForestClassifier(n_jobs=-1, class_weight="balanced", max_depth=5)
+    boruta = BorutaPy(rf, random_state=2205, n_estimators="auto")
+    boruta.fit(X_tmp, y_tmp)
+    boruta_res = boruta.ranking_
+
+    results_raw = pd.DataFrame(
+        {
+            "variable": [f"V{i}" for i in range(1, p + 1)],
+            "auc": auc_scores,
+            "mutual_info": mi_scores,
+            "f_statistic": f_scores,
+            "f_pvalue": f_pvalues,
+            "boruta": boruta_res,
+        }
+    )
+
+    results = results_raw.copy()
+
+    for col in ["auc", "mutual_info", "f_statistic"]:
+        results[f"rank_{col}"] = results[col].rank(ascending=False)
+
+    for col in ["boruta"]:
+        results[f"rank_{col}"] = results[col].rank(ascending=True)
+
+    results["mean_rank"] = results[
+        ["rank_auc", "rank_mutual_info", "rank_f_statistic", "rank_boruta"]
+    ].mean(axis=1)
+    results = results.sort_values("mean_rank").reset_index(drop=True)
+    results["overall_rank"] = results.index + 1
+    top = results.head(top)
+
+    if return_raw:
+        return top, results_raw
+    results.to_csv("feature_importance_rank.csv", index=False)
+    return top
+
+
+if __name__ == "__main__":
+    X = pd.read_csv("/Users/ola/projects/cost-sensitive-marketing/data/x_train.txt", sep = " ")
+    y = pd.read_csv("/Users/ola/projects/cost-sensitive-marketing/data/y_train.txt", sep = " ")
+    y = y.values.ravel()
+    res = info_features(X, y)
